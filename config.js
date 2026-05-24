3f5b8859f9c7a8b105fd0ca427e60f3a678ad084
kevinxramirezx21-maker <kevinxramirezx21@icloud.com>
2026-05-24 02:42:21 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+// ==========================================
+// SATURN CONFIG
+// ==========================================
+
+export const CONFIG = {
+  RPC_URL:
+    "https://mainnet.helius-rpc.com/?api-key=YOUR_HELIUS_KEY",
+
+  COINGECKO_API:
+    "https://api.coingecko.com/api/v3",
+
+  DEXSCREENER_API:
+    "https://api.dexscreener.com/latest/dex",
+
+  JUPITER_PRICE_API:
+    "https://price.jup.ag/v4",
+
+  JUPITER_QUOTE_API:
+    "https://quote-api.jup.ag/v6",
+
+  SOL_MINT:
+    "So11111111111111111111111111111111111111112",
+};
