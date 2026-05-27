e0e6fb3dbc7150c8ef6a7d5b57b7a44b42e15919
kevinxramirezx21-maker <kevinxramirezx21@icloud.com>
2026-05-26 23:18:59 -0700

    Create saturn-config.js

---
+++ js/saturn-config.js
@@ -0,0 +1,6 @@
+window.SATURN_CONFIG = {
+  RPC_URL: "https://mainnet.helius-rpc.com/?api-key=PASTE_YOUR_NEW_HELIUS_KEY_HERE",
+  FEE_WALLET: "CqXPQf6Qe2D2KJ5UYvTLi3prAwQiSntJUXZmE3knyzFb",
+  JUPITER_FEE_BPS: 20,
+  NETWORK: "mainnet-beta"
+};
